1d40c523332ec86ea6262480951d0d05693129e8
pooyafils <pooyafils@gmail.com>
2022-02-07 12:38:27 +0330

    RemoveDelicateElements

---
+++ src/RemoveDelicateElements.java
@@ -0,0 +1,16 @@
+public class RemoveDelicateElements {
+    public static String removeDuplicates(String str) {
+        char[] chArray = str.toCharArray(); // or, use charAt(i)
+        StringBuilder sb = new StringBuilder();
+        for (char ch : chArray) {
+            if (sb.indexOf(String.valueOf(ch)) == -1) {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String test=":java:";
+        System.out.println(removeDuplicates(test));
+    }
+}
