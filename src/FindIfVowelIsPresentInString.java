2935f89b22841d086af442e6b1114de8cce0ffcb
pooyafils <pooyafils@gmail.com>
2022-03-08 00:33:31 +0330

    FindIfVowelIsPresentInString

---
+++ src/FindIfVowelIsPresentInString.java
@@ -0,0 +1,14 @@
+import java.sql.Statement;
+
+public class FindIfVowelIsPresentInString {
+    public static boolean vowelIsPresent(String givenString,String vowel){
+        if(givenString.contains(vowel)){
+            return true;
+        }
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+System.out.println(vowelIsPresent("this is","is"));
+    }
+}
