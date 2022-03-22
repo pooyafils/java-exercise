f0efeb985d657fab6e60ff4c0f5f4aaa2493fcf9
pooyafils <pooyafils@gmail.com>
2022-03-22 14:14:01 +0430

    sort hashmap, merge two list and date formatter

---
+++ src/DateFormat.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormat {
+    public static void main(String[] args) {
+
+        String patternt="MM-dd-YYY hh:mm:ss";
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat(patternt);
+        String date=simpleDateFormat.format(new Date());
+        System.out.println(date);
+    }
+}
