677bd199effbdc9df06417b46c9780342c66d3e8
pooyafils <pooyafils@gmail.com>
2022-04-10 08:44:04 +0430

    custom exceptions and logging

---
+++ src/java9recipes/chapter9/CreatingExceptionClass/CreatingExceptionClass.java
@@ -0,0 +1,8 @@
+package java9recipes.chapter9.CreatingExceptionClass;
+
+public class CreatingExceptionClass extends RuntimeException {
+    CreatingExceptionClass (String message) {
+        super(message);
+    }
+
+}
