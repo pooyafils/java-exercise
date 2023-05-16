c3307e4a712830a6e0331ddb1534399372adbdfd
pooyafils <pooyafils@gmail.com>
2023-05-16 09:15:00 -0700

    BucketSort

---
+++ src/interview/BucketSortMain.java
@@ -0,0 +1,22 @@
+package interview;
+
+import java.util.Arrays;
+
+public class BucketSortMain {
+    public static void main(String[] args) {
+
+        // initialize an array integers
+        int[] integers = {4, 1, 8, 3, 8, 2, 6, 7, 4, 9};
+
+        System.out.println("\n\nSort via Bucket sort:");
+        System.out.println("-------------------------");
+
+        int[] integersC1 = integers.clone();
+        int[] integersC2 = integers.clone();
+
+        BucketSort.sort1(integersC1);
+        BucketSort.sort1(integersC2);
+        System.out.println("Sorted C1: " + Arrays.toString(integersC1));
+        System.out.println("Sorted C2: " + Arrays.toString(integersC2));
+    }
+}
