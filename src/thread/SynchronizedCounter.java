52c0aaa421c767d4ccd598a495648f318013c1e5
pooyafils <pooyafils@gmail.com>
2023-10-18 17:36:27 +0330

    SynchronizedCounter

---
+++ src/thread/SynchronizedCounter.java
@@ -0,0 +1,43 @@
+package thread;
+
+public class SynchronizedCounter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SynchronizedCounter counter = new SynchronizedCounter();
+
+        // Create and start multiple threads to increment the counter
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        // Print the final counter value
+        System.out.println("Final Counter Value: " + counter.getCount());
+    }
+}
