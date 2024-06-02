e56302dc295f8c221de88b28724779ec0b8c4974
pooyafils <pooyafils@gmail.com>
2024-06-02 13:19:36 +0330

    create of thread

--- src/masteringthreadcourse/chapterthree/CreateThread.java
+++ src/masteringthreadcourse/chapterthree/CreateThread.java
@@ -1,10 +1,48 @@
 package masteringthreadcourse.chapterthree;
 
 public class CreateThread {
-    public static void main(String[] args) throws InterruptedException {
+  /*  public static void main(String[] args) throws InterruptedException {
        Thread thread= Thread.currentThread();
        System.out.println("current thread "+thread.getName());
        Thread.sleep(3000);
        System.out.println("current thread "+thread.getName());
+    }*/
+
+  /*  public static void main(String[] args) {
+        MyThread myThread=new MyThread();
+        myThread.start(); // use start method not run method
+    }
+
+    static class MyThread extends Thread{
+        public void run(){
+            setName("my Thread");
+            System.out.println("current thread "+Thread.currentThread().getName());
+        }
+    }*/
+
+/*    public static void main(String[] args) {
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("current thread "+Thread.currentThread().getName());
+            }
+        };
+        Thread thread=new Thread(runnable);
+        thread.setName("run");
+        thread.start();
+
+    }*/
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("{1} current thread "+Thread.currentThread().getName());
+        Thread thread=new Thread(
+                ()->{
+                    System.out.println("{2} current thread"+ Thread.currentThread().getName());
+                }
+        );
+        thread.setName("my thread ");
+        thread.start();
+        thread.join();
+        System.out.println("{3} current thread "+Thread.currentThread().getName());
     }
 }
