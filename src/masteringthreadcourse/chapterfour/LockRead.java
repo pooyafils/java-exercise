5a7d3f540e56d3c3c2189dccc118cd0c5adc6a22
pooyafils <pooyafils@gmail.com>
2024-06-10 10:42:01 +0330

    read lock

--- src/masteringthreadcourse/chapterfour/LockRead.java
+++ src/masteringthreadcourse/chapterfour/LockRead.java
@@ -1,7 +1,53 @@
 package masteringthreadcourse.chapterfour;
 
-public class LockRead {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class LockRead {
+    private static  int S=0;
+    private static int[] array=new int[10];
+    private static Lock lock=new ReentrantLock();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            array[i] = 10;
+
+        }
+        List<Thread> threadList = new ArrayList<>();
+        int threadSlice = array.length / 2;
+        for (int i = 0; i < 2; i++) {
+            Thread t = new Thread(new WorkerThread(i * threadSlice, (i + 1) * threadSlice));
+            t.start();
+            threadList.add(t);
+
+
+        }
+        threadList.forEach(t->{
+            try{
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.println("the sum is "+S);
+    }
+    static class WorkerThread implements Runnable{
+        private final int left;
+        private final int right;
+
+        public WorkerThread(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+        @Override
+        public void run() {
+            for(int i=left;i<right;i++){
+                lock.unlock();
+                S=S+array[i];
+                lock.unlock();
+            }
+        }
     }
 }
